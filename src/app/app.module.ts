ce0aff830381ba6ae46dc25f052a8861bd339eec
Carla Iten <carlaiten@gmail.com>
2020-02-16 20:44:24 +0100

    adding header and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { CvComponent } from './cv/cv.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
     CvComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
