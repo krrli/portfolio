a4e81c63630ebae3f146eaf29743e866f141cbf9
Carla Iten <carlaiten@gmail.com>
2020-05-27 12:19:26 +0200

    added animations to fade and slide in and out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,9 +38,10 @@ import { ProjectDetailSliderComponent } from './project-detail-slider/project-de
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
-    NgbModalModule
+    NgbModalModule,
+    BrowserAnimationsModule
   ],
-  exports:[ProjectDetailComponent],
+  exports: [ProjectDetailComponent],
   providers: [],
   bootstrap: [AppComponent],
   entryComponents: [ProjectDetailComponent]
