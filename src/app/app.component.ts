7ac8c0ff331d4f9d1bb956ad05e12d123718eae8
Carla Iten <carlaiten@gmail.com>
2020-06-24 15:51:54 +0200

    trying this thing with google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'portfolio';
+
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+       if (event instanceof NavigationEnd) {
+           gtag('config', 'UA-169833563-1',
+                 {
+                   page_path: event.urlAfterRedirects
+                 }
+                );
+        }
+     }
+  ); }
+
 }
