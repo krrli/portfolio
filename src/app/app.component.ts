5a77fe7b9af3b7141536f14bcb4254501dad2434
Carla Iten <carlaiten@gmail.com>
2020-06-24 16:51:47 +0200

    update meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Router, NavigationEnd} from '@angular/router';
+import { Title, Meta } from '@angular/platform-browser';
 
 declare let gtag: Function;
 
@@ -9,10 +10,10 @@ declare let gtag: Function;
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+  title = 'Portfolio Carla Iten';
 
-  constructor(public router: Router) {
+  constructor(public router: Router, private titleService: Title, private metaService: Meta) {
     this.router.events.subscribe(event => {
        if (event instanceof NavigationEnd) {
            gtag('config', 'UA-169833563-1',
@@ -23,5 +24,15 @@ export class AppComponent {
         }
      }
   ); }
+
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Portfolio, Software Engineer'},
+      {name: 'description', content: 'Hallo! Ich bin Carla, eine Software Ingenieurin aus Luzern. Ich mag Code, ein tolles Team, glückliche User und Kund:innen, ...'},
+      {name: 'robots', content: 'index, follow'},
+      {name: 'image', content: 'https://carla.land/assets/carla-bw.jpg'}
+    ]);
+  }
 
 }
