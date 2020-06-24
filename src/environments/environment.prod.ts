71327aad3488d5a1a3a0928cfd0ac79daa584655
Carla Iten <carlaiten@gmail.com>
2020-06-24 15:16:27 +0200

    adding prod firebase and mail

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAUGeIXpJ_ry4IrLU57M3Y-JNQrrMid-Ng",
+    authDomain: "portfolio-4a24f.firebaseapp.com",
+    databaseURL: "https://portfolio-4a24f.firebaseio.com",
+    projectId: "portfolio-4a24f",
+    storageBucket: "portfolio-4a24f.appspot.com",
+    messagingSenderId: "284043347460"
+  }
 };
